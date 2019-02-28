4c1b8419b3768691147fa3f0273dde8336a7551e
Vinicius Apolinario <vinicius.ap@live.com>
2019-02-28 16:07:50 -0300

    Create Form Seller

--- SalesWebMVC/Controllers/ProductsController.cs
+++ SalesWebMVC/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
